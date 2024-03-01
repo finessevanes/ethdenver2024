bc9fc54e67b4cd4df694f09b41ab6c19f1c5b748
Xian Zheng <seeinplay@gmail.com>
2024-03-01 10:23:30 -0700

    use harpie test api key

--- src/app/components/Harpie.tsx
+++ src/app/components/Harpie.tsx
@@ -1,15 +1,21 @@
-export default function Harpie() {
-  fetch("https://api.harpie.io/v2/validateAddress", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      apiKey: process.env.NEXT_PUBLIC_URL,
-      address: "0x55456cbd1f11298b80a53c896f4b1dc9bc16c731",
-    }),
-  });
+import { useEffect } from "react";
+
+export default function HarpieComponent() {
+  useEffect(() => {
+    const reponse = fetch("https://api.harpie.io/v2/validateAddress", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        apiKey: "74778fa4-88a8-4e35-922a-02bd82005edd",
+        address: "0x55456cbd1f11298b80a53c896f4b1dc9bc16c731",
+      }),
+    }).then((response) => {
+      console.log(response);
+    });
+  }, []);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
