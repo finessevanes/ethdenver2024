2b3e868562c6bc117fa7514cfcb39d415e485435
Vanessa Mercado <vanessa.mercado24@gmail.com>
2024-03-02 15:28:39 -0700

    remove unused files

--- src/contract/utils.ts
+++ src/contract/utils.ts
@@ -7,32 +7,3 @@ async function getEthAmountForUsd(
   const amountInEth = await contract.ethToUsd(amountInUsd);
   return amountInEth;
 }
-
-// Send ETH to the smart contract and mint an NFT
-async function donateAndMintNFT(
-  amountInUsd: number,
-  contractAddress: string,
-  abi: any,
-  userAddress: string
-): Promise<void> {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  await provider.send("eth_requestAccounts", []);
-  const signer = provider.getSigner();
-
-  const contract = new ethers.Contract(contractAddress, abi, signer);
-
-  // Get the amount in ETH
-  const amountInEth = await getEthAmountForUsd(amountInUsd, contract);
-
-  // Send ETH to the contract
-  const tx = await signer.sendTransaction({
-    to: contractAddress,
-    value: ethers.utils.parseEther(amountInEth),
-  });
-  await tx.wait();
-
-  const mintTx = await contract.mintNFT(userAddress); // Replace with your contract's minting function
-  await mintTx.wait();
-}
-
-export default donateAndMintNFT;
